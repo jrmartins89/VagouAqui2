ecaf6319f5aa1cccc40dfb410c50c94b8b853ed5
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-08-07 21:14:22 -0300

    added params for user fields

--- db/userModel.js
+++ db/userModel.js
@@ -1,1 +1,15 @@
 const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, "Please provide an Email!"],
+        unique: [true, "Email Exist"],
+    },
+
+    password: {
+        type: String,
+        required: [true, "Please provide a password!"],
+        unique: false,
+    },
+})
