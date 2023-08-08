93630eb022a47b39e1ed0ed2f0508ed15b571039
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-08-07 23:25:28 -0300

    added free and auth endpoints to app.

--- app.js
+++ app.js
@@ -112,4 +112,14 @@ app.post("/login", (request, response) => {
         });
 });
 
+// free endpoint
+app.get("/free-endpoint", (request, response) => {
+    response.json({ message: "You are free to access me anytime" });
+});
+
+// authentication endpoint
+app.get("/auth-endpoint", (request, response) => {
+    response.json({ message: "You are authorized to access me" });
+});
+
 module.exports = app;
