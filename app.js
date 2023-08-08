c995ffeff5298276b2b4479bce23af8b9462f2c0
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-08-07 23:14:41 -0300

    added login endpoint to app

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.get("/", (request, response, next) => {
 // execute database connection
 dbConnect();
 
+// register endpoint
 app.post("/register", (request, response) => {
 // hash the password
   bcrypt
@@ -53,6 +54,62 @@ app.post("/register", (request, response) => {
           e,
         });
       });
+});
+
+// login endpoint
+app.post("/login", (request, response) => {
+// check if email exists
+    User.findOne({ email: request.body.email })
+
+        // if email exists
+        .then((user) => {
+            // compare the password entered and the hashed password found
+            bcrypt
+                .compare(request.body.password, user.password)
+
+                // if the passwords match
+                .then((passwordCheck) => {
+
+                    // check if password matches
+                    if(!passwordCheck) {
+                        return response.status(400).send({
+                            message: "Passwords does not match",
+                            error,
+                        });
+                    }
+
+                    //   create JWT token
+                    const token = jwt.sign(
+                        {
+                            userId: user._id,
+                            userEmail: user.email,
+                        },
+                        "RANDOM-TOKEN",
+                        { expiresIn: "24h" }
+                    );
+
+                    //   return success response
+                    response.status(200).send({
+                        message: "Login Successful",
+                        email: user.email,
+                        token,
+                    });
+                })
+                // catch error if password does not match
+                .catch((error) => {
+                    response.status(400).send({
+                        message: "Passwords does not match",
+                        error,
+                    });
+                });
+        })
+        // catch error if email does not exist
+        .catch((e) => {
+            response.status(404).send({
+                message: "Email not found",
+                e,
+            });
+        });
 });
 
 module.exports = app;
